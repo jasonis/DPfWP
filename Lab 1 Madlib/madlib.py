457ccf203b9d06b4f79bfe63921292c42ebc64dd
jasonis <jislocum78@gmail.com>
2014-05-07 23:02:04 -0500

    getting started on madlib

---
+++ Lab 1 Madlib/madlib.py
@@ -0,0 +1,9 @@
+# Jason Slocum
+# 5.7.2014
+# Lab 1 Madlib
+
+current_year = 2014
+current_age = response = input("How old are you currently?")
+fav_movie = response = raw_input("What is your favorite movie?")
+movie_year = response = input("What year did it come out?")
+age = []
