848cb018a4788290e85da0316525aef4032efd83
jasonis <jislocum78@gmail.com>
2014-05-08 20:55:14 -0500

    array

--- Lab 1 Madlib/madlib.py
+++ Lab 1 Madlib/madlib.py
@@ -35,3 +35,9 @@ elif frequency < 5:
     print "You definitely do like " + str(fav_movie) + " ! At that rate, you have seen it approximately " + str(lv) + " times in your life!"
 else:
     print "Wow! You sure do love " + str(fav_movie) + " ! At that rate, you have seen it approximately " + str(lv) + " times in your life!"
+
+foods = ["popcorn", "nachos", "candy"]
+
+your_foods = response = raw_input("A couple of common movie foods are " + str(foods[0]) + " and " + str(foods[2]) + ". " " What else do you like?")
+
+foods.append(your_foods)
