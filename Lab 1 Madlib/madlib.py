15756c88d6abb75a2465138c9078e5bc026f7c48
jasonis <jislocum78@gmail.com>
2014-05-08 20:42:52 -0500

    removed age[]

--- Lab 1 Madlib/madlib.py
+++ Lab 1 Madlib/madlib.py
@@ -6,4 +6,3 @@ current_year = 2014
 current_age = response = input("How old are you currently?")
 fav_movie = response = raw_input("What is your favorite movie?")
 movie_year = response = input("What year did it come out?")
-age = []
