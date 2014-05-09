bfd3b268771512f59a0938d3d4e7a743e874c773
jasonis <jislocum78@gmail.com>
2014-05-08 20:45:10 -0500

    created calculate age function

--- Lab 1 Madlib/madlib.py
+++ Lab 1 Madlib/madlib.py
@@ -6,3 +6,9 @@ current_year = 2014
 current_age = response = input("How old are you currently?")
 fav_movie = response = raw_input("What is your favorite movie?")
 movie_year = response = input("What year did it come out?")
+
+def calculate_age(cy, my, ca):
+    movie_age = cy - my
+    your_age = ca - movie_age
+    return your_age
+a = calculate_age(current_year, movie_year, current_age)
