1df8111999f2b4e6ef96931afc57fea86ac53b3d
jasonis <jislocum78@gmail.com>
2014-05-10 00:05:22 -0500

    page head

--- Lab 2 Server Side Form/form/main.py
+++ Lab 2 Server Side Form/form/main.py
@@ -8,7 +8,13 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         page_head = """
-
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Simple Form</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" ?>
+    </head>
+    <body>"""
         page_body = """
 
         page_close = """
