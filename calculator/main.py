5c9d68a3605e2a91f8b4e8fa806fa77ada7d6bd7
jasonis <jislocum78@gmail.com>
2014-05-13 20:32:55 -0500

    page body

--- calculator/main.py
+++ calculator/main.py
@@ -11,6 +11,19 @@ class MainHandler(webapp2.RequestHandler):
         <link href="css/style.css" rel="stylesheet" type"text/css" />
     </head>
     <body>"""
+        page_body = """
+        <h1>Family Water Consumption</h1>
+        <div>
+            <ul>
+                <li>Jason</li>
+                <li>Katie</li>
+                <li>Isaac</li>
+                <li>Ella</li>
+                <li>Chewie</li>
+                <li>Otis</li>
+            </ul>
+        </div>
+"""
 
 
 app = webapp2.WSGIApplication([
