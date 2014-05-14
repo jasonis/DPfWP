9ca8159ab14d2931f344bd3b013e8c25c3392acd
jasonis <jislocum78@gmail.com>
2014-05-13 21:31:04 -0500

    total water calculator

--- calculator/main.py
+++ calculator/main.py
@@ -7,55 +7,55 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = Page()
 
-        j = Family_Member()
-        j.name = "Jason"
-        j.status = "father"
-        j.water_morning = 50
-        j.water_afternoon = 30
-        j.water_evening = 30
-        #j.total_water()
+        ji = Family_Member()
+        ji.name = "Jason"
+        ji.status = "father"
+        ji.water_morning = 50
+        ji.water_afternoon = 30
+        ji.water_evening = 30
+        ji.total_water()
 
-        k = Family_Member()
-        k.name = "Katie"
-        k.status = "mother"
-        k.water_morning = 30
-        k.water_afternoon = 30
-        k.water_evening = 20
-        #k.total_water()
+        ka = Family_Member()
+        ka.name = "Katie"
+        ka.status = "mother"
+        ka.water_morning = 30
+        ka.water_afternoon = 30
+        ka.water_evening = 20
+        ka.total_water()
 
-        i = Family_Member()
-        i.name = "Isaac"
-        i.status = "son"
-        i.water_morning = 20
-        i.water_afternoon = 20
-        i.water_evening = 40
-        #i.total_water()
+        ij = Family_Member()
+        ij.name = "Isaac"
+        ij.status = "son"
+        ij.water_morning = 20
+        ij.water_afternoon = 20
+        ij.water_evening = 40
+        #ij.total_water()
 
-        e = Family_Member()
-        e.name = "Ella"
-        e.status = "daughter"
-        e.water_morning = 10
-        e.water_afternoon = 20
-        e.water_evening = 20
-        #e.total_water()
+        ea = Family_Member()
+        ea.name = "Ella"
+        ea.status = "daughter"
+        ea.water_morning = 10
+        ea.water_afternoon = 20
+        ea.water_evening = 20
+        ea.total_water()
 
-        c = Family_Member()
-        c.name = "Chewie"
-        c.status = "dog1"
-        c.water_morning = 3
-        c.water_afternoon = 4
-        c.water_evening = 2
-        #c.total_water()
+        ch = Family_Member()
+        ch.name = "Chewie"
+        ch.status = "dog1"
+        ch.water_morning = 3
+        ch.water_afternoon = 4
+        ch.water_evening = 2
+        ch.total_water()
 
-        o = Family_Member()
-        o.name = "Otis"
-        c.status = "dog2"
-        o.water_morning = 4
-        o.water_afternoon = 4
-        o.water_evening = 3
-        #o.total_water()
+        ot = Family_Member()
+        ot.name = "Otis"
+        ot.status = "dog2"
+        ot.water_morning = 4
+        ot.water_afternoon = 4
+        ot.water_evening = 3
+        ot.total_water()
 
-        family = [j, k, i, e, c, o]
+        family = [ji, ka, ij, ea, ch, ot]
 
         if self.request.GET:
             self.response.write(p.page_head + p.page_body + p.page.close)
@@ -71,6 +71,9 @@ class Family_Member(object):
         self.water_afternoon = 0
         self.water_evening = 0
         #self.total_water = 0
+
+    def total_water(self):
+        self.__water_consumed = self.water_morning + self.water_afternoon + self.water_evening
 
 
 app = webapp2.WSGIApplication([
