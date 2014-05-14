80531aa48ab3d8e189079276bdbd29615b0ede6f
jasonis <jislocum78@gmail.com>
2014-05-13 21:02:33 -0500

    page.py

--- calculator/main.py
+++ calculator/main.py
@@ -6,30 +6,6 @@ from p import Page
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = Page()
-        page_head = """
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <title>Family Water Consumption</title>
-        <link href="css/style.css" rel="stylesheet" type"text/css" />
-    </head>
-    <body>"""
-        page_body = """
-        <h1>Family Water Consumption</h1>
-        <div>
-            <ul>
-                <li><button>Jason</button></li>
-                <li><button>Katie</button></li>
-                <li><button>Isaac</button></li>
-                <li><button>Ella</button></li>
-                <li><button>Chewie</button></li>
-                <li><button>Otis</button></li>
-            </ul>
-        </div>
-"""
-        page_close = """
-    </body>
-</html>"""
 
         j = Family_Member()
         j.name = "Jason"
