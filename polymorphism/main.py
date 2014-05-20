332b6d0f5e382460b54b95c20b2098284af21e68
jasonis <jislocum78@gmail.com>
2014-05-19 22:59:23 -0500

    polymorphism

---
+++ polymorphism/main.py
@@ -0,0 +1,56 @@
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        p = FormPage()
+        p.inputs = [['first_name', 'text', 'First Name'], ['last_name', 'text', 'Last Name'], ['Submit', 'submit']]
+        self.response.write(p.print_out_form())
+
+class Page(object):
+    def __init__(self):
+        self._head ="""
+<!DOCTYPE>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>"""
+        self._body ='Filler'
+        self._close ="""
+    </body>
+</html>"""
+    def print_out(self):
+        return self._head + self._body + self._close
+
+class FormPage(Page):
+    def __init__(self):
+        super(FormPage, self).__init__()#Page.__init__()
+        self._form_open = '<form method="GET">'
+        self._form_close = '</form>'
+        self.__inputs = []
+        self._form_inputs = ''
+
+
+    @property
+    def inputs(self):
+        pass
+
+    @inputs.setter
+    def inputs(self, arr):
+        self.__inputs = arr
+
+        for item in arr:
+            self._form_inputs += '<input type="' + item[1] + '" name="' + item[0]
+            try:
+                self._form_inputs += '"placeholder="' + item[2] + '" />'
+            except:
+                self._form_inputs += '" />'
+    #polymorphism 
+    def print_out(self):
+        return self._head + self._body + self._form_open + self._form_inputs + self._form_close + self._close
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
