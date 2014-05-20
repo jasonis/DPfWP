8793995221001581ba89fe8ed4bc69a2f3ec77b0
jasonis <jislocum78@gmail.com>
2014-05-19 22:41:24 -0500

    inheritance and abstract

--- inheritance/main.py
+++ inheritance/main.py
@@ -5,7 +5,7 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = FormPage()
         p.inputs = [['first_name', 'text', 'First Name'], ['last_name', 'text', 'Last Name'], ['Submit', 'submit']]
-        self.response.write(p.print_out())
+        self.response.write(p.print_out_form())
 
 class Page(object):
     def __init__(self):
@@ -27,8 +27,10 @@ class FormPage(Page):
     def __init__(self):
         super(FormPage, self).__init__()#Page.__init__()
         self._form_open = '<form method="GET">'
-        self._form_open = '</form>'
+        self._form_close = '</form>'
         self.__inputs = []
+        self._form_inputs = ''
+
 
     @property
     def inputs(self):
@@ -39,8 +41,14 @@ class FormPage(Page):
         self.__inputs = arr
 
         for item in arr:
-            print item
+            self._form_inputs += '<input type="' + item[1] + '" name="' + item[0]
+            try:
+                self._form_inputs += '"placeholder="' + item[2] + '" />'
+            except:
+                self._form_inputs += '" />'
 
+    def print_out_form(self):
+        return self._head + self._body + self._form_open + self._form_inputs + self._form_close + self._close
 
 
 app = webapp2.WSGIApplication([
