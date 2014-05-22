caacaa5cb3e47aa78708687e2d3c69370273377f
jasonis <jislocum78@gmail.com>
2014-05-21 21:00:11 -0500

    dog object

--- foxsays/main.py
+++ foxsays/main.py
@@ -1,9 +1,14 @@
 
 import webapp2
 
+from page import Page
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         page = Page()
+
+        dog = Canis()
+        dog.name = 'Yorkshire Terrier'
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
