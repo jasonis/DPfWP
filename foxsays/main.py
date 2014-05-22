b15407e06c4e07a24b67b66ec35aa1bf28fcb472
jasonis <jislocum78@gmail.com>
2014-05-21 21:10:13 -0500

    getters

--- foxsays/main.py
+++ foxsays/main.py
@@ -31,6 +31,30 @@ class abstract_animal(object):
         self._phy = 'Chordata'
         self._say = ''
 
+    @property
+    def phy(self):
+        return self._phy
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def avg_life(self):
+        return self._avg_life
+
+    @property
+    def hab(self):
+        return self._hab
+
+    @property
+    def geo(self):
+        return self._geo
+
+    @property
+    def say(self):
+        return self._say
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
