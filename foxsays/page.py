c729cf6f57d1d3a042af4a039a36d9b3ca984195
jasonis <jislocum78@gmail.com>
2014-05-21 21:02:25 -0500

    page header

--- foxsays/page.py
+++ foxsays/page.py
@@ -1,2 +1,17 @@
 class Page():
     def __init__(self):
+                self.__header = '''<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Wisconsin Zoological Society</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Griffy' rel='stylesheet' type='text/css'>
+    </head>
+    <header>
+        <h1>Wisconsin Zoological Society</h1>
+        <h1 id='h1bg'>Wisconsin Zoological Society</h1>
+        <div>
+            <input placeholder="Search for an Animal"></input
+        </div>
+    </header>
+    <body>'''
