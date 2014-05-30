a83bc4194bab1241d57ab7ada819d3caacfd465b
jasonis <jislocum78@gmail.com>
2014-05-29 20:02:16 -0500

    page.py basic html

--- final/pages.py
+++ final/pages.py
@@ -5,3 +5,26 @@ DPW
 Final API
 """
 
+class Page(object):
+    def __init__(self):
+        self._head ="""
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Belong</title>
+    
+    </head>
+    <header>
+        <ul>
+            <li class="logolink"><a href='http://zillow.com'>Search Homes</a></li>
+            <li id="logo">Belong</li>
+            <li class="logolink"><a href=''>About Us</a></li>
+        </ul>
+    </header>
+    <body>"""
+        self._body ='Property Info'
+        self._close ="""
+    </body>
+</html>"""
+    def print_out(self):
+        return self._head + self._body + self._close
