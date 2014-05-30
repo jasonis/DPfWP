7a8604cdbf5f83793118ef6013a46d818d7c74bc
jasonis <jislocum78@gmail.com>
2014-05-29 20:06:35 -0500

    for commit

--- final/main.py
+++ final/main.py
@@ -40,6 +40,16 @@ class FormPage(Page):
     def inputs(self, arr):
         self.__inputs = arr
 
+        for item in arr:
+            self._form_inputs += '<input type="' + item[1] + '" name="' + item[0]
+            try:
+                self._form_inputs += '" placeholder="' + item[2] + '" />'
+            except:
+                self._form_inputs += '" />'
+
+    def print_out(self):
+        return self._head + self._form_open + self._form_inputs + self._form_close + self._body + self._close
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
